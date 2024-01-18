1c75195d984a0d027e05aa1dbb862a588176e130
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-01-18 15:11:11 -0500

    modified greeting textview font size to 30sp

--- ICE2/app/src/main/java/ca/georgiancollege/comp3025_w2024_week_2/MainActivity.kt
+++ ICE2/app/src/main/java/ca/georgiancollege/comp3025_w2024_week_2/MainActivity.kt
@@ -2,10 +2,18 @@ package ca.georgiancollege.comp3025_w2024_week_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val greetingTextView = findViewById<TextView>(R.id.greetingTextView)
+        greetingTextView.text = "Hello Nick"
+
+        Log.i("onCreate", "This is on create")
+        println("hello world")
     }
 }
