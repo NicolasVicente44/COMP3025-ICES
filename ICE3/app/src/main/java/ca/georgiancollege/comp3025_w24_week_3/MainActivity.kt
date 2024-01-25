32ab52c04e0d056004ec9c1d800654ee51c4d83d
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-01-25 14:44:14 -0500

    added resultview shared event handler for modifiying the text view

--- ICE3/app/src/main/java/ca/georgiancollege/comp3025_w24_week_3/MainActivity.kt
+++ ICE3/app/src/main/java/ca/georgiancollege/comp3025_w24_week_3/MainActivity.kt
@@ -2,10 +2,31 @@ package ca.georgiancollege.comp3025_w24_week_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        val percentageButton = findViewById<Button>(R.id.percentageButton)
+
+        clearButton.setOnClickListener{view -> processOperatorButtons(view)}
+        percentageButton.setOnClickListener{view -> processOperatorButtons(view)}
+
     }
+
+    private fun processOperatorButtons(view: View)
+    {
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        Log.i("operators", view.tag.toString())
+        resultTextView.text = view.tag.toString()
+    }
+
 }
