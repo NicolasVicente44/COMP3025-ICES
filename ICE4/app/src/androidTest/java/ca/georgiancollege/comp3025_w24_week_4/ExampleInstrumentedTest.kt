98b434ee9ac4908e575be135a20bb6371cb37b46
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-01 14:30:31 -0500

    cloned ice 3 into ice 4

--- ICE4/app/src/androidTest/java/ca/georgiancollege/comp3025_w24_week_4/ExampleInstrumentedTest.kt
+++ ICE4/app/src/androidTest/java/ca/georgiancollege/comp3025_w24_week_4/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package ca.georgiancollege.comp3025_w24_ice4
+package ca.georgiancollege.comp3025_w24_week_4
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ca.georgiancollege.comp3025_w24_ice4", appContext.packageName)
+        assertEquals("ca.georgiancollege.comp3025_w24_week_3", appContext.packageName)
     }
 }
