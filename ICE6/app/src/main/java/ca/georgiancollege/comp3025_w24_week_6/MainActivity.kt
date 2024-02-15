01714d61383064475034587cae7599c9595d3906
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-02-15 17:11:08 -0500

    added data manager singleton

--- ICE6/app/src/main/java/ca/georgiancollege/comp3025_w24_week_6/MainActivity.kt
+++ ICE6/app/src/main/java/ca/georgiancollege/comp3025_w24_week_6/MainActivity.kt
@@ -23,29 +23,5 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    private fun  getTextFromResource(context: Context, resourceId: Int): String {
-        return context.resources.openRawResource(resourceId)
-            .bufferedReader()
-            .use { it.readText() }
-    }
-
-
-    private fun getTextFromAsset(context: Context, fileName: String): String {
-        return context.resources.assets.open(fileName)
-            .bufferedReader()
-            .use { it.readText() }
-    }
-
-
-    private fun deserializeJSON(): List<ContactModel>? {
-        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
-        val listType = Types.newParameterizedType(List::class.java, ContactModel::class.java)
-        val adapter: JsonAdapter<List<ContactModel>> = moshi.adapter(listType)
-
-        val contactListRawString = getTextFromResource(this, R.raw.contacts)
-        val contactList: List<ContactModel>? = adapter.fromJson(contactListRawString)
-        return contactList
-    }
-
 
 }
