e6f4489c862c4fb0b550a486b65196469cdb69e2
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-03-07 14:53:16 -0500

    updated main acitivty file

--- ICE7/app/src/main/java/ca/georgiancollege/comp3025_w24_week_8/MainActivity.kt
+++ ICE7/app/src/main/java/ca/georgiancollege/comp3025_w24_week_8/MainActivity.kt
@@ -1,12 +1,34 @@
-package ca.georgiancollege.comp3025_w24_week_8
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import ca.georgiancollege.comp3025_w24_week_8.FirstAdapter
+import ca.georgiancollege.comp3025_w24_week_8.TVShow
+import ca.georgiancollege.comp3025_w24_week_8.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // Declare an instance of the binding class
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Inflate the layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val favouriteTVShows = arrayOf(
+            TVShow("House of the Dragon", "HBO"),
+            TVShow("Lord of the Rings", "Prime Video"),
+            TVShow("Andor", "Disney"),
+            TVShow("Severance", "AppleTv"),
+            TVShow("Star Trek: Strange New Worlds", "Paramount+")
+        )
+
+        val firstAdapter = FirstAdapter(favouriteTVShows)
+
+        // Use view binding to replace findViewById or synthetic properties
+        binding.FirstRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = firstAdapter
+        }
     }
 }
-
