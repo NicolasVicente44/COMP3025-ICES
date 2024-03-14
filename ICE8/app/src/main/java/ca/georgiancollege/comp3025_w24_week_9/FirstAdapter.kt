719bdf7cc937614b1973a6e217a12a5ce4dc5eed
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-03-14 15:10:27 -0400

    updated the first adapter

--- ICE8/app/src/main/java/ca/georgiancollege/comp3025_w24_week_9/FirstAdapter.kt
+++ ICE8/app/src/main/java/ca/georgiancollege/comp3025_w24_week_9/FirstAdapter.kt
@@ -5,23 +5,30 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import ca.georgiancollege.comp3025_w24_week_9.databinding.TextRowItemBinding
 
-class FirstAdapter(private val dataSet: List<MovieModel>?) :
-    RecyclerView.Adapter<FirstAdapter.ViewHolder>() {
+class FirstAdapter(private var dataSet: List<Movie>) :
+    RecyclerView.Adapter<FirstAdapter.ViewHolder>()
+{
+    var onMovieClick: ((Movie)-> Unit)? = null
 
     class ViewHolder(val binding: TextRowItemBinding) : RecyclerView.ViewHolder(binding.root)
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder
+    {
         // Inflate the layout with view binding
-        val binding =
-            TextRowItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        val binding = TextRowItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int)
+    {
         // Use view binding to set the text
-        viewHolder.binding.title.text = dataSet!![position].title
+        viewHolder.binding.title.text = dataSet[position].title
         viewHolder.binding.studio.text = dataSet[position].studio
+
+        viewHolder.binding.root.setOnClickListener{
+            onMovieClick?.invoke(dataSet[position])
+        }
     }
 
-    override fun getItemCount() = dataSet!!.size
+    override fun getItemCount() = dataSet.size
 }
