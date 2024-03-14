127e99d92bc0f5040d44ca1555e88a6e53d1ed33
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-03-14 16:12:01 -0400

    added firestore datamanager class

--- ICE8/app/build.gradle.kts
+++ ICE8/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -59,4 +60,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    implementation("com.google.firebase:firebase-common-ktx:20.4.2")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.3")
 }
---
+++ ICE8/app/src/main/java/ca/georgiancollege/comp3025_w24_week_9/FirebaseMovie.kt
@@ -0,0 +1,6 @@
+package ca.georgiancollege.comp3025_w24_week_9
+
+data class FirebaseMovie(
+    val title: String = "",
+    val studio: String = ""
+)
