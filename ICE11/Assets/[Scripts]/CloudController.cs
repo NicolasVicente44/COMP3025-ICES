8917609a477798f31b627a80c02594256755129d
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-04-06 15:02:24 -0400

    clouds working

--- ICE11/Assets/[Scripts]/CloudController.cs
+++ ICE11/Assets/[Scripts]/CloudController.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IslandController : MonoBehaviour
+public class CloudController : MonoBehaviour
 {
     public float verticalSpeed;
+    public float topVeticalSpeed;
+    public float lowestVerticalSpeed;
     public float topBound;
     public float bottomBound;
     public float leftBound;
     public float rightBound;
+    public float horizontalDrift;
 
     // Start is called before the first frame update
     void Start()
     {
         ResetGameObject();
-
     }
 
     // Update is called once per frame
@@ -26,13 +28,26 @@ public class IslandController : MonoBehaviour
 
     void Move()
     {
-        transform.position -= new Vector3(0, verticalSpeed, 0);
+        
+        float moveX = 0; 
+        float moveY = verticalSpeed;
+
+        
+        moveX += horizontalDrift;
+
+        // Update position
+        transform.position -= new Vector3(moveX, moveY, 0);
     }
 
     void ResetGameObject()
     {
         var randomX = Random.Range(leftBound, rightBound);
         transform.position = new Vector3(randomX, topBound, 0);
+
+        verticalSpeed = Random.Range(lowestVerticalSpeed, topVeticalSpeed);
+
+        float driftAmount = Random.Range(-horizontalDrift, horizontalDrift);
+        transform.position += new Vector3(driftAmount, 0, 0);
     }
 
 
