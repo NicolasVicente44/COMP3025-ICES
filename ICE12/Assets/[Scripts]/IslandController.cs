7fcdfa1a89e5e619d2dea3c2635baff394ac4581
Nick Vicente <nicolas.a.vicente44@gmail.com>
2024-04-11 13:33:28 -0400

    added ice 12

--- ICE12/Assets/[Scripts]/IslandController.cs
+++ ICE12/Assets/[Scripts]/IslandController.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloudController : MonoBehaviour
+public class IslandController : MonoBehaviour
 {
     public float verticalSpeed;
-    public float topVeticalSpeed;
-    public float lowestVerticalSpeed;
     public float topBound;
     public float bottomBound;
     public float leftBound;
     public float rightBound;
-    public float lowestDrift;
-    public float highestDrift;
-    public float driftAmount;
 
     // Start is called before the first frame update
     void Start()
     {
         ResetGameObject();
+
     }
 
     // Update is called once per frame
@@ -30,27 +26,14 @@ public class CloudController : MonoBehaviour
 
     void Move()
     {
-        float moveX = driftAmount; // Horizontal movement
-        float moveY = verticalSpeed; // Vertical movement
-
-        // Update position
-        transform.position -= new Vector3(moveX, moveY, 0);
+        transform.position -= new Vector3(0, verticalSpeed, 0);
     }
-
 
     void ResetGameObject()
     {
-        float randomX = Random.Range(leftBound, rightBound);
-
-        randomX = Mathf.Clamp(randomX, leftBound, rightBound);
-
+        var randomX = Random.Range(leftBound, rightBound);
         transform.position = new Vector3(randomX, topBound, 0);
-
-        verticalSpeed = Random.Range(lowestVerticalSpeed, topVeticalSpeed);
-        driftAmount = Random.Range(lowestDrift, highestDrift);
     }
-
-
 
 
     void CheckBounds()
